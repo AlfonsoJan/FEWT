c78551012257cce004e8e9b851b82e7482404fce
Jan Alfonso <alfonsobusker@gmail.com>
2023-05-24 20:40:22 +0200

    finished week 4

--- Week 4/plugins/enzyme.js
+++ Week 4/plugins/enzyme.js
@@ -2,9 +2,12 @@ const fp = require('fastify-plugin');
 const fs = require('fs');
 const util = require('util');
 
+// Convert fs.readFile into Promise version of same    
+const readFile = util.promisify(fs.readFile);
+
 
 function addToExisting(enzyme) {
-    let enzymeName = enzyme[0];
+    let enzymeName = enzyme[0].toUpperCase();
     let site;
     if (enzyme.length == 2) {
         site = enzyme[1];
@@ -20,11 +23,11 @@ let enzymeContainer = {
         return this.enzymes.get(enzym);
     },
     addEnzyme(enzyme) {
-        if (this.enzymes.has(enzyme[0])) {
+        let enzymeName = enzyme[0].toUpperCase();
+        if (this.enzymes.has(enzymeName)) {
             addToExisting(enzyme)
             return;
         }
-        let enzymeName = enzyme[0];
         if (enzyme.length == 2) {
             let site = enzyme[1];
             this.enzymes.set(enzymeName, {"site": [site], "synonym": ""});
@@ -34,10 +37,36 @@ let enzymeContainer = {
         let site = enzyme[2];
         this.enzymes.set(enzymeName, {"site": [site], "synonym": synonym});
     },
+    set sequence(seq) {
+        this.seq = seq;
+    },
+    getCuttingSites() {
+        let result = []
+        for (let [enzyme, value] of this.enzymes) {
+            let site = value.site.map(s => {
+                return s
+                    .replace("^", "")
+                    .replace(/B/g, "[CGT]")
+                    .replace(/D/g, "[AGT]")
+                    .replace(/H/g, "[ACT]")
+                    .replace(/K/g, "[GT]")
+                    .replace(/M/g, "[AC]")
+                    .replace(/N/g, "[ACGT]")
+                    .replace(/R/g, "[AG]")
+                    .replace(/S/g, "[CG]")
+                    .replace(/V/g, "[ACG]")
+                    .replace(/W/g, "AT")
+                    .replace(/Y/g, "[CT]");
+            })
+            let index = [...this.seq.matchAll(new RegExp(site, 'g'))].map(hit => hit.index);
+            if (index.length > 0) {
+                result.push({enzym: enzyme, "site": site, "locations": index})
+            }
+        }
+        return result;
+    }
 }
 
-// Convert fs.readFile into Promise version of same    
-const readFile = util.promisify(fs.readFile);
 
 async function readData(fileName) {
     const data = await readFile(fileName, 'utf8');
@@ -47,8 +76,8 @@ async function readData(fileName) {
 function parseData(data) {
     data
         .split('\n')
-        .map(row => row.split(/\s+/))
-        .slice(1)
+        .map(row => row.trim().split(/\s+/))
+        .slice(1, -2)
         .forEach(element => {
             if (!(element.length < 2)) {
                 enzymeContainer.addEnzyme(element)
@@ -62,7 +91,6 @@ async function getEnzymeContainer() {
     let data = await readData("./plugins/data/restriction_enzymes_table-rebase.txt");
     parseData(data);
     return enzymeContainer;
-    
 }
 
 
--- Week 4/routes/root.js
+++ Week 4/routes/root.js
@@ -2,14 +2,34 @@
 
 module.exports = async function (fastify, opts) {
 
+  fastify.addContentTypeParser('text/json', { parseAs: 'string' }, fastify.getDefaultJsonParser('ignore', 'ignore'))
+
+  fastify.decorate('notFound', (request, reply, text, status) => {
+    let error = {}
+    error[status] = text;
+    reply.code(status).type('application/json').send(error)
+  })
+
+  fastify.setNotFoundHandler(fastify.notFound)
+
   const enzymeContainer = await fastify.enzymeContainer();
 
   fastify.get('/api/v3/enzyme/:enzymeName', async function (request, reply) {
     const { enzymeName } = request.params;
-    const result = enzymeContainer.getEnzym(enzymeName)
+    const result = enzymeContainer.getEnzym(enzymeName.toUpperCase());
     if (result === undefined) {
-      return {"Not found": "In the database"};
+      return fastify.notFound(request, reply, "Not found in the database!", 404);
     }
-    return {"enzyme name": enzymeName, "synonym": result.synonym, "site": result.site};
+    return {"enzyme name": enzymeName.toUpperCase(), "synonym": result.synonym, "site": result.site};
+  })
+
+
+  fastify.post('/api/v3/getcuttingsites', async function (request, reply) {
+    if (request.body.oligo.length < 10 | request.body.oligo.length > 100) {
+      return fastify.notFound(request, reply, "Length of the sqeuence should between 10 and 100", 500);
+    }
+    enzymeContainer.sequence = request.body.oligo;
+    let result = enzymeContainer.getCuttingSites();
+    return result;
   })
 }
